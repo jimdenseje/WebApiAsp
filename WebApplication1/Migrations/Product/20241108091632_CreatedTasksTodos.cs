62d5399874ea3716103a3f1dc880ea77e118d524
Jim Haaland Damgaard <jimdenseje@gmail.com>
2024-11-08 11:56:16 +0100

    added SQL database

---
+++ WebApplication1/Migrations/Product/20241108091632_CreatedTasksTodos.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiAsp.Migrations.Product
+{
+    /// <inheritdoc />
+    public partial class CreatedTasksTodos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InStock = table.Column<int>(type: "int", nullable: false),
+                    Sold = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductItems", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductItems");
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -6,10 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
-builder.Services.AddDbContext<ProductContext>(opt =>
-    opt.UseInMemoryDatabase("ProductList"));
+//builder.Services.AddDbContext<TodoContext>(opt =>
+//    opt.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddDbContext<ProductContext>(opt =>
+//    opt.UseInMemoryDatabase("ProductList"));
+
+var connectionString = builder.Configuration.GetConnectionString("TodoItems_WebApiDBConnectionString");
+builder.Services.AddDbContext<TodoContext>(o => o.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ProductContext>(o => o.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +24,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+} else
+{
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
